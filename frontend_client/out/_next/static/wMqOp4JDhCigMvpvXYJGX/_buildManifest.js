1923e0588ab36cf8514db008ecd90c2334cb5d8b
Paramjeet Singh <dapp.programmer@gmail.com>
2022-04-10 16:58:16 +0530

    added out folder

---
+++ frontend_client/out/_next/static/wMqOp4JDhCigMvpvXYJGX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0a6e12db-5cf3d70e9d4903a0.js","static/chunks/905-d25f05cb13f04ca3.js","static/chunks/pages/index-5295a23067c4f270.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
